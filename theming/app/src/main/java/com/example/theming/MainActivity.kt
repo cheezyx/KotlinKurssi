7f5444e1fbba66e8705d9aa8aa3d985df1d0564d
cheezyx <cheezyx@users.noreply.github.com>
2024-09-27 15:41:53 +0300

    week4

---
+++ theming/app/src/main/java/com/example/theming/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.theming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.theming.ui.theme.ThemingTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ThemingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    App(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(modifier: Modifier = Modifier){
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "My Title",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = appModifier
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {/**/},
+            modifier = appModifier
+        )
+
+        Button(
+            onClick = {/* */},
+            modifier = appModifier
+        ) {
+            Text(text = "Submit")
+        }
+        Button(
+            onClick = {/**/},
+            modifier = appModifier
+        ) {
+            Text(text = "Practice button")
+        }
+    }
+}
+@Preview
+@Composable
+fun PreviewApp() {
+    ThemingTheme {
+        App()
+    }
+}
